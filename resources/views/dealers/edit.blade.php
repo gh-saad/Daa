dce87406e90ae9060ea591b5fb8397f63ae6fdd4
JamalSetup970 <158152749+JamalSetup970@users.noreply.github.com>
2024-04-19 10:07:43 +0500

    v4

--- resources/views/dealers/edit.blade.php
+++ resources/views/dealers/edit.blade.php
@@ -483,4 +483,18 @@
 @endsection
 
 @push('scripts')
+<script>
+    function previewImage(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#dealer-logo-preview')
+                    .attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+</script>
 @endpush
